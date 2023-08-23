3fd591095e45a2c8a37616cb012328fce1ae35be
IgnacioToledoDev <igna.toledot@gmail.com>
2023-08-23 19:25:30 -0400

    add create comment with firebase

--- src/pages/comments.jsx
+++ src/pages/comments.jsx
@@ -1,11 +1,15 @@
 import Title from "../components/Title";
-import { TestimonialCard } from "./coment";
+import { TestimonialCard } from "../components/coment";
+import { AddComment } from "../components/addComment";
 
 export const Comments = () => {
   return (
     <div className="p-4 ga-3 pt-12" id="comments">
       <Title title="Comentarios" />
-      <div className="p-8 flex flex-row flex-wrap w-full justify-between m-2">
+      <div className="w-full h-full flex items-center justify-center">
+        <AddComment />
+      </div>
+      <div className="space-y-[6px] py-4 flex flex-row flex-wrap w-full justify-between">
         <TestimonialCard />
         <TestimonialCard />
         <TestimonialCard />
