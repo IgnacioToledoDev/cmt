21881f7e06d190295f24ed060d5ee09549c61562
IgnacioToledoDev <igna.toledot@gmail.com>
2023-09-11 13:21:56 -0300

    remove files

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Quiz } from "./pages/Quiz.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
-import { Admin } from "./pages/Admin.jsx";
-import { Dashboard } from "./pages/Dashboard";
 import { Questions } from "./pages/Questions.jsx";
 
 const router = createBrowserRouter([
@@ -23,16 +21,6 @@ const router = createBrowserRouter([
   {
     path: "/questions",
     element: <Questions />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/admin/login",
-    element: <Admin />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/admin/dashboard",
-    element: <Dashboard />,
     errorElement: <ErrorPage />,
   },
 ]);
